cb5e6847372e265604784106b21b8443b355c58a
seunghyun <51603029+kimu2370@users.noreply.github.com>
2019-12-19 14:56:43 +0900

    updateMINI-POS 19/12/19

--- data.js
+++ data.js
@@ -1,22 +1,23 @@
-var DATA = [
-    {id : "Aid", name : "자몽", price : "3,800"},
-    {id : "Aid", name : "청포도", price : "3,800"},
-    {id : "Aid", name : "레몬", price : "3,800"},
-    {id : "Aid", name : "수박", price : "3,800"},
-    {id : "TEA", name : "복숭아", price : "2,800"},
-    {id : "TEA", name : "레몬", price : "2,800"},
-    {id : "TEA", name : "녹차", price : "2,800"},
-    {id : "TEA", name : "홍차", price : "2,800"},
-    {id : "TEA", name : "캐모마일", price : "2,800"},
-    {id : "TEA", name : "얼그레이", price : "2,800"},
-    {id : "TEA", name : "페퍼민트", price : "2,800"},
-    {id : "HOT coffee", name : "아메리카노", price : "2,800"},
-    {id : "HOT coffee", name : "카페라떼", price : "3,200"},
-    {id : "HOT coffee", name : "카페모카", price : "3,500"},
-    {id : "HOT coffee", name : "카푸치노", price : "3,200"},
-    {id : "ICED coffee", name : "(ICE)아메리카노", price : "2,800"},
-    {id : "ICED coffee", name : "(ICE)카페라떼", price : "3,200"},
-    {id : "ICED coffee", name : "(ICE)카페모카", price : "3,500"},
-    {id : "ICED coffee", name : "(ICE)카푸치노", price : "3,200"},
+// localStorge를 이용해 보기
+
+let DATA = [
+    {id : "coffee", name : "아메리카노", price : 3500},
+    {id : "coffee", name : "카페라떼", price : 4000},
+    {id : "coffee", name : "카페모카", price : 4500},
+    {id : "coffee", name : "카푸치노", price : 3500},
+    {id : "coffee", name : "아포가토", price : 4500},
+    {id : "coffee", name : "카페비엔나", price : 4000},
+    {id : "coffee", name : "더치커피", price : 4000},
+    {id : "coffee", name : "청포도에이드", price : 3500},
+    {id : "coffee", name : "오렌지에이드", price : 3500},
+    {id : "coffee", name : "딸기에이드", price : 3500},
+    {id : "bread", name : "옥수수빵", price : 1500},
+    {id : "bread", name : "소보로빵", price : 1500},
+    {id : "bread", name : "슈크림빵", price : 2000},
+    {id : "bread", name : "크림치즈빵", price : 2000},
+    {id : "bread", name : "마늘 바게뜨", price : 3500},
+    {id : "bread", name : "야채고로케", price : 1500},
+    {id : "bread", name : "고기고로케", price : 2000},
+    {id : "bread", name : "크로아상", price : 3000}
 ];
-var cart = [];
+let cart = [];
--- pos.js
+++ pos.js
@@ -1,72 +1,102 @@
-// 자바스크립트 파일
-/*
-pseudo code
-1.각 카테고리 버튼을 클릭하면 주제에 맞는 메뉴가 나온다.
-2.출력 화면에 메뉴를 누를 때마다 같은 메뉴이면 수량이 쌓이고, 다른 메뉴이면 그 밑으로 쌓이게 한다.
-3.메뉴가 화면에 채워 질때마다 총 수량, 주문 금액, 청구 금액, 남은 금액의 수가 바뀌어야 한다.
-4.만약 수량을 바꾸고 싶으면 +버튼,-버튼을 눌러서 수량을 바꿀 수 있다.(advenced 나중에 번호입력도 만들어보자)
-5.주문한 메뉴 하나를 클릭하게 되면 "주문을 취소 하시겠습니까?" 라는 내용을 보여주고 확인을 누르면 주문한 메뉴를 뺀다.(append되어 있던 것을 제거 한다.)
-6.만약 다시 주문해야 되는 상황이 생기면 clear를 통해 주문 리스트(cart)에 있는 내용을 모두 지우고 다시 채우게 한다.
-*/
-let category_btn = document.querySelectorAll('.category-btn');
-let isClick = false; // 버튼이 눌렸는지 체크
-let carrier; // 랜더링한 메뉴를 담을 변수
+// selector
+let orderContentArea = document.querySelector('#orderContent');
+let orderTotalArea = document.querySelector('#orderTotal');
+let menuCategory = document.querySelectorAll('#menuCategory>button');
+let menuArea = document.querySelector('#menuItems');
 
-//button 클릭 시 menu들을 랜더링함.
-category_btn.forEach(function(item,i){
-    category_btn[i].onclick = function(){
-        if(isClick){ // 처음 랜더링 이후 반복 될 조건문
-            clear(carrier); // 카테고리 별 버튼을 한번 클리어 시킨후 랜더링 한다.
-            randeringMenu();
-        }else{// 처음 랜더링 할 메뉴
-            carrier = randeringMenu();
-        } 
-    }
-});
+//event
+menuCategory.forEach(node=>node.addEventListener('click',clickedCategoryFn));
 
-function randeringMenu(){
-    isClick = true; // 카테고리 버튼을 눌렀는지 체크 한다.
-    let item = filteringCategory(event.target.textContent);
-    let menu_item = document.querySelector('#menu-item');
-    for(let i=0; i<item.length;i++){//카테고리 클릭 시 메뉴 항목들을 menu-template로 생성한다.
-        let menu_template = document.querySelector('#menu-template');
-        let menu = document.importNode(menu_template.content,true);
-        menu_item.appendChild(menu);
-        let menu_name = document.querySelectorAll('.menu-name');
-        let menu_price = document.querySelectorAll('.menu-price');
-        menu_name[i].textContent = item[i].name;
-        menu_price[i].textContent = item[i].price;
-    }
-    /*
-        출력 화면에 클릭한 메뉴를 생성하기 위해 각 class들을 가져온다.
-    */
-    let menu_btn = document.querySelectorAll('.menu-btn');
-    menu_btn.forEach(function(item,i){
-        let count = 0;
-        menu_btn[i].onclick = function(e){
-            count=count+1;
-            let show_table = document.querySelector('#show-table');
-            let order_template = document.querySelector('#order-template');
-            let order = document.importNode(order_template.content,true);
-            show_table.appendChild(order);
-            let order_num = document.querySelector('.num');
-            let order_menu = document.querySelector('.menu');
-            let order_quantity = document.querySelector('.quantity').querySelector('.q-value');
-            let order_price = document.querySelector('.price');
-            order_num.textContent =count;
-            order_menu.textContent = e.path[1].childNodes[0].textContent;
-            // order_quantity = 
-            order_price.textContent = e.path[1].childNodes[1].textContent;
-        }
-    });
-    return menu_item;
+//카테고리 클릭 이벤트 함수
+function clickedCategoryFn(event){
+   //DATA[index].name의 값을 얻기 위해 클릭된 카테고리의 클래스 이름을 가져온다.
+   let clickedCategoryName = event.target.className;
+   //기존의 메뉴 클리어
+   clear(menuArea);
+   createMenuFn(clickedCategoryName);
 }
-//DATA에서 필터링 한 카테고리 id
-function filteringCategory(filteredID){
-    return DATA.filter((el)=>{
-        return el.id === filteredID;
-    });
+
+//메뉴 생성 함수
+function createMenuFn(categoryName){
+   let menus = DATA.filter(menu=>(menu.id===categoryName));
+   menus.forEach((element)=>{
+      let button = document.createElement('button');
+      button.innerText = element.name;
+      menuArea.append(button);
+   });
+   let menuItems = document.querySelectorAll('#menuItems>button');
+   menuItems.forEach(node=>node.addEventListener('click',clickedMenuFn));
+
 }
-function clear(target){
-    target.innerHTML = "";
+
+//메뉴를 클릭하면 카트에 담는 함수
+function clickedMenuFn(event){
+   let clickedMenuName = event.target.textContent;
+   let menuInfo = DATA.filter(info=>(info.name===clickedMenuName))[0];//배열을 벗겨낸 객체 데이터 할당.
+   let cartInfo = cart.find(info=>info.name===menuInfo.name);
+   if(!cartInfo){//카트에 메뉴 이름이 없으면
+      menuInfo["num"] = cart.length+1;
+      menuInfo["quantity"] = 1;
+      cart.push(menuInfo); //번호와 수량을 담은 객체를 카트배열에 푸시
+      createList();
+      sumPriceFn(menuInfo);
+   }else{//카트에 메뉴 이름이 있으면
+      addQuantity(cartInfo);
+      listReload();
+      sumPriceFn(cartInfo);
+   }
+   orderTotalLenderFn();
+}
+
+//리스트 생성 함수
+function createList(){
+   let div = document.createElement('div');
+   orderContentArea.append(div);
+   for(let i=0; i<4; i++){
+      let span = document.createElement('span');
+      div.append(span);
+   }
+   let spans = document.querySelectorAll('#orderContent>div')[cart.length-1].children;
+      spans[0].innerText = cart[cart.length-1].num;
+      spans[1].innerText = cart[cart.length-1].name;
+      spans[2].innerText = cart[cart.length-1].price;
+      spans[3].innerText = cart[cart.length-1].quantity;
+}
+
+//해당 카트의 수량 증가 함수
+function addQuantity(cartInfo){
+   cartInfo.quantity = cartInfo.quantity+1;
+   document.querySelectorAll('#orderContent>div').forEach((list)=>{
+      if(list.children[1].textContent===cartInfo.name){
+         list.lastElementChild.innerText = cartInfo.quantity;
+      }
+   });
+}
+
+//리스트 리로드 함수
+function listReload(){
+   let backUpList = orderContentArea.innerHTML;
+   clear(orderContentArea);
+   orderContentArea.innerHTML = backUpList;
+}
+
+//클릭한 메뉴에 대한 총 금액 연산 함수
+function sumPriceFn(info){
+   let currentSumPrice = 0;
+   currentSumPrice= currentSumPrice + info.price*info.quantity;
+   info["sumPrice"]=currentSumPrice;   //cart에 키:값으로 저장
+}
+
+//총 금액 과 총 수량 렌더 함수
+function orderTotalLenderFn(){
+   let totalQuantity = cart.map(x=>x.quantity).reduce((acc,cur)=>(acc+cur));
+   let totalPrice = cart.map(x=>x.sumPrice).reduce((acc,cur)=>acc+cur);
+   orderTotalArea.querySelectorAll('p')[0].textContent = totalPrice;
+   orderTotalArea.querySelectorAll('p')[1].textContent = totalQuantity;
+   
+}
+
+//클리어 함수
+function clear(area){
+   area.innerHTML = "";
 }
